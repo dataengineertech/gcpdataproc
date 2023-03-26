ecdd79e88817326d08ef124ef47b9a718681f9ee
Rakesh Tripathy <admin@dataengineertech.com>
2023-03-27 00:34:54 +0530

    group by

--- examples/beam_test_code_new/src/main/java/org/example/Main.java
+++ examples/beam_test_code_new/src/main/java/org/example/Main.java
@@ -5,14 +5,19 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.join.CoGroupByKey;
+import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.TypeDescriptors;
+import org.checkerframework.checker.units.qual.K;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
@@ -62,7 +67,7 @@ public class Main {
                         return s.length();
                     }
                 }));*/
-        PCollection<String> lines = pipeline.apply(Create.of("Hello World1", "Beam is fun"));
+       /* PCollection<String> lines = pipeline.apply(Create.of("Hello World1", "Beam is fun"));
         PCollection<String> lineLengths = lines.apply(MapElements.via(
                 new SimpleFunction<String, String>() {
                     @Override
@@ -72,10 +77,86 @@ public class Main {
                         return String.valueOf(line.length());
                     }
                 }));
-        lineLengths.apply(TextIO.write().to("gs://beam_test_123/output/map"));
+        lineLengths.apply(TextIO.write().to("gs://beam_test_123/output/map"));*/
+
+        //groupbykey -start
+        /*PCollection<KV<String, String>> mapped = pipeline.apply(Create.of(KV.of("cat", "1"), KV.of("dog", "2"), KV.of("dog", "3"), KV.of("and", "4"), KV.of("and", "5")));
+        PCollection<KV<String, Iterable<String>>> reduced =
+                mapped.apply(GroupByKey.<String, String>create());
+        PCollection<Map<String, ArrayList<String>>> grp = reduced.apply(ParDo.of(new DoFn<KV<String, Iterable<String>>,Map<String, ArrayList<String>>>(){
+            @ProcessElement
+            public void processElement(ProcessContext c) {
+                //KV<String, Iterable<String>> word = c.element();
+                Map<String, ArrayList<String>> outputmap = new HashMap<String, ArrayList<String>>();
+                String key = c.element().getKey();
+                Iterable<String> val = c.element().getValue();
+                System.out.println(key);
+                for(String s : val){
+                    System.out.println(s);
+                    if (outputmap.containsKey(key)){
+                        outputmap.get(key).add(s);
+                    }
+                    else{
+                        ArrayList<String> valarr = new ArrayList<>();
+                        valarr.add(s);
+                        outputmap.put(key, valarr);
+                    }
+                }
+                c.output(outputmap);
+                //Integer length = word.length();
+
+            }}));
+        PCollection<String> op = grp.apply(ParDo.of(new DoFn<Map<String, ArrayList<String>>, String>(){
+            @ProcessElement
+            public void processElement(ProcessContext c) {
+                Map<String, ArrayList<String>> outputmap1 = c.element();
+                String text_file_op = "";
+                for(Map.Entry<String, ArrayList<String>> entry: outputmap1.entrySet()) {
+                    String key_val = entry.getKey() + " "+String.join(", ", entry.getValue());
+                    text_file_op = text_file_op+"\n"+key_val;
+                }
+            c.output(text_file_op);
+
+        }}));
+
+        op.apply(TextIO.write().to("gs://beam_test_gr/output"));*/
+
+        //groupbykey -end
+
+        //cogroupbykey - start
+        PCollection<KV<String, Integer>> pt1 = pipeline.apply(Create.of(KV.of("apple", 1), KV.of("banana", 2), KV.of("apple", 9), KV.of("orange", 7), KV.of("banana", 7)));
+        PCollection<KV<String, String>> pt2 = pipeline.apply(Create.of(KV.of("apple", "kashmir"), KV.of("banana", "maha"), KV.of("orange", "MP")));
+
+        final TupleTag<Integer> t1 = new TupleTag<>();
+        final TupleTag<String> t2 = new TupleTag<>();
+        PCollection<KV<String, CoGbkResult>> result =
+                KeyedPCollectionTuple.of(t1, pt1).and(t2, pt2)
+                        .apply(CoGroupByKey.create());
+        PCollection<String> op = result.apply(ParDo.of(new DoFn<KV<String, CoGbkResult>, String>() {
+            @ProcessElement
+            public void processElement(ProcessContext c) {
+                KV<String, CoGbkResult> e = c.element();
+                CoGbkResult result = e.getValue();
+                // Retrieve all integers associated with this key from pt1
+                Iterable<Integer> allIntegers = result.getAll(t1);
+                for (int i : allIntegers){
+                    System.out.println(i);
+                }
+                // Retrieve the string associated with this key from pt2.
+                // Note: This will fail if multiple values had the same key in pt2.
+                //String string = e.getOnly(t2);
+                Iterable<String> allString = result.getAll(t2);
+                for (String s: allString){
+                    System.out.println(s);
+                }
+                c.output("hello");
+            }}));
+        //}
+
 
         pipeline.run();
 
 
-    }
+
 }
+    }
